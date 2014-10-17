8b6e679bf9250cf5cbff58bd251be6791ed165e9
Julian Hangstörfer <jhangstoerfer@gmail.com>
2014-10-17 20:24:01 +0200

    Added "".format to library

--- InstaSynchP-Library.user.js
+++ InstaSynchP-Library.user.js
@@ -17,6 +17,26 @@
 // ==/UserScript==
 
 if (!window.pluginLibrary) {
+    //http://joquery.com/2012/string-format-for-javascript
+    if (typeof String.prototype.format !== 'function') {
+        String.prototype.format = function () {
+            // The string containing the format items (e.g. "{0}")
+            // will and always has to be the first argument.
+            var theString = this,
+                i,
+                regEx;
+
+            // start with the second argument (i = 1)
+            for (i = 0; i < arguments.length; i += 1) {
+                // "gm" = RegEx options for Global search (more than one instance)
+                // and for Multiline search
+                regEx = new RegExp("\\{" + (i) + "\\}", "gm");
+                theString = theString.replace(regEx, arguments[i]);
+            }
+            return theString;
+        };
+    }
+    //http://stackoverflow.com/a/646643
     if (typeof String.prototype.startsWith !== 'function') {
         // see below for better implementation!
         String.prototype.startsWith = function (str) {
