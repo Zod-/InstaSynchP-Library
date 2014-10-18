dececa670a599bce2aeab2a459f0cfc2be70f2ed
Julian Hangstörfer <jhangstoerfer@gmail.com>
2014-10-18 17:57:25 +0200

    Don't execute only once

--- InstaSynchP-Library.user.js
+++ InstaSynchP-Library.user.js
@@ -3,7 +3,7 @@
 // @namespace   InstaSynchP
 // @description Basic function that are needed by several scripts use with @require
 
-// @version     1.0.4
+// @version     1.0.5
 // @author      Zod-
 // @source      https://github.com/Zod-/InstaSynchP-Library
 // @license     GPL-3.0
@@ -16,106 +16,104 @@
 // @run-at      document-start
 // ==/UserScript==
 
-if (!window.pluginLibrary) {
-    //http://joquery.com/2012/string-format-for-javascript
-    if (typeof String.prototype.format !== 'function') {
-        String.prototype.format = function () {
-            // The string containing the format items (e.g. "{0}")
-            // will and always has to be the first argument.
-            var theString = this,
-                i,
-                regEx;
+//http://joquery.com/2012/string-format-for-javascript
+if (typeof String.prototype.format !== 'function') {
+    String.prototype.format = function () {
+        // The string containing the format items (e.g. "{0}")
+        // will and always has to be the first argument.
+        var theString = this,
+            i,
+            regEx;
 
-            // start with the second argument (i = 1)
-            for (i = 0; i < arguments.length; i += 1) {
-                // "gm" = RegEx options for Global search (more than one instance)
-                // and for Multiline search
-                regEx = new RegExp("\\{" + (i) + "\\}", "gm");
-                theString = theString.replace(regEx, arguments[i]);
-            }
-            return theString;
-        };
-    }
-    //http://stackoverflow.com/a/646643
-    if (typeof String.prototype.startsWith !== 'function') {
-        // see below for better implementation!
-        String.prototype.startsWith = function (str) {
-            return this.indexOf(str) === 0;
-        };
-    }
-
-    window.isBlackname = function (username) {
-        "use strict";
-        if (typeof username !== 'string') {
-            return false;
+        // start with the second argument (i = 1)
+        for (i = 0; i < arguments.length; i += 1) {
+            // "gm" = RegEx options for Global search (more than one instance)
+            // and for Multiline search
+            regEx = new RegExp("\\{" + (i) + "\\}", "gm");
+            theString = theString.replace(regEx, arguments[i]);
         }
-        return username.match(/^([A-Za-z0-9]|([\-_](?![\-_]))){5,16}$/) !== null;
+        return theString;
     };
+}
+//http://stackoverflow.com/a/646643
+if (typeof String.prototype.startsWith !== 'function') {
+    // see below for better implementation!
+    String.prototype.startsWith = function (str) {
+        return this.indexOf(str) === 0;
+    };
+}
 
-    window.activeVideoIndex = function () {
-        "use strict";
-        return $('#playlist .active').index();
-    };
-
-    window.findUserId = function (id) {
-        "use strict";
-        var i;
-        for (i = 0; i < window.users.length; i += 1) {
-            if (id === window.users[i].id) {
-                return window.users[i];
-            }
-        }
-        return undefined;
-    };
-    window.findUserName = function (name) {
-        "use strict";
-        var i;
-        for (i = 0; i < window.users.length; i += 1) {
-            if (name === window.users[i].name) {
-                return window.users[i];
-            }
-        }
-        return undefined;
-    };
-
-    window.videojs = function () {
-        "use strict";
-        return $('.video-js')[0];
-    };
-
-    window.reloadPlayer = function () {
-        "use strict";
-        if (window.video) {
-            window.video.destroy();
-        }
-        window.global.sendcmd('reload', null);
-    };
-
-    window.addSystemMessage = function (message) {
-        "use strict";
-        window.addMessage({
-            username: ""
-        }, message, 'system');
-    };
-
-    window.addErrorMessage = function (message) {
-        "use strict";
-        window.addMessage({
-            username: ""
-        }, message, 'errortext');
-    };
-
-    window.videoInfoEquals = function (a, b) {
-        "use strict";
-        if (!a || !b) {
-            return false;
-        }
-        if (a.provider && a.provider === b.provider &&
-            a.mediaType && a.mediaType === b.mediaType &&
-            a.id && a.id === b.id) {
-            return true;
-        }
+function isBlackname(username) {
+    "use strict";
+    if (typeof username !== 'string') {
         return false;
-    };
-    window.pluginLibrary = true;
+    }
+    return username.match(/^([A-Za-z0-9]|([\-_](?![\-_]))){5,16}$/) !== null;
+}
+
+function activeVideoIndex() {
+    "use strict";
+    return $('#playlist .active').index();
+}
+
+function findUserId(id) {
+    "use strict";
+    var i;
+    for (i = 0; i < window.users.length; i += 1) {
+        if (id === window.users[i].id) {
+            return window.users[i];
+        }
+    }
+    return undefined;
+}
+
+function findUserName(name) {
+    "use strict";
+    var i;
+    for (i = 0; i < window.users.length; i += 1) {
+        if (name === window.users[i].name) {
+            return window.users[i];
+        }
+    }
+    return undefined;
+}
+
+function videojs() {
+    "use strict";
+    return $('.video-js')[0];
+}
+
+function reloadPlayer() {
+    "use strict";
+    if (window.video) {
+        window.video.destroy();
+    }
+    window.global.sendcmd('reload', null);
+}
+
+function addSystemMessage(message) {
+    "use strict";
+    window.addMessage({
+        username: ""
+    }, message, 'system');
+}
+
+function addErrorMessage(message) {
+    "use strict";
+    window.addMessage({
+        username: ""
+    }, message, 'errortext');
+}
+
+function videoInfoEquals(a, b) {
+    "use strict";
+    if (!a || !b) {
+        return false;
+    }
+    if (a.provider && a.provider === b.provider &&
+        a.mediaType && a.mediaType === b.mediaType &&
+        a.id && a.id === b.id) {
+        return true;
+    }
+    return false;
 }
